e3469ceff4c181429525aa4b07cf905a1b0f968d
Dmitriy Grachev <dmitriy.grachev@erlyvideo.org>
2022-01-14 11:53:00 +0300

    removed example project #21709

--- example/app/src/main/java/com/erlyvideo/sample/Adapters/GlideStubModule.java
+++
@@ -1,8 +0,0 @@
-package com.erlyvideo.sample.Adapters;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public class GlideStubModule extends AppGlideModule {
-}
